b1d9186c000442eccca9054c76063e9b4317efa0
Cap Kello <captainkello@gmail.com>
2015-03-16 23:43:27 -0700

    Added custom adapter view

--- app/src/main/java/com/codepath/apps/tweetclient/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/tweetclient/TweetsArrayAdapter.java
@@ -1,9 +1,15 @@
 package com.codepath.apps.tweetclient;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.tweetclient.models.Tweet;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -13,5 +19,25 @@ public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
         super(context, android.R.layout.simple_list_item_1, tweets);
     }
 
-
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO: Implement the ViewHolder Pattern = 3-16-2015
+        // Get the tweet
+        Tweet tweet = getItem(position);
+        // Find or inflate the template
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
+        }
+        // find subviews to populate
+        TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
+        TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+        ImageView ivProfileImage = (ImageView) convertView.findViewById(R.id.ivProfileImage);
+        // Populate data into the subviews
+        tvUserName.setText(tweet.getUser().getScreenName());
+        tvBody.setText(tweet.getBody());
+        // Use Picasso library to load and cache image
+        ivProfileImage.setImageResource(android.R.color.transparent); // Clear the previous image for a recycled view
+        Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).into(ivProfileImage);
+        return convertView;
+    }
 }
